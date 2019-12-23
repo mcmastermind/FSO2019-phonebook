b4f13d0cfd7cd8e994e08994c8bcd67cdf482d05
KingKode <richard.mcmaster@live.com>
2019-12-22 22:42:44 -0600

    Changes to be committed: 	new file:   build/asset-manifest.json 	new file:   build/favicon.ico 	new file:   build/index.html 	new file:   build/logo192.png 	new file:   build/logo512.png 	new file:   build/manifest.json 	new file:   build/precache-manifest.1d272981a9d3f405d01dfd8f524438e4.js 	new file:   build/robots.txt 	new file:   build/service-worker.js 	new file:   build/static/css/main.eae4d89b.chunk.css 	new file:   build/static/css/main.eae4d89b.chunk.css.map 	new file:   build/static/js/2.39360e90.chunk.js 	new file:   build/static/js/2.39360e90.chunk.js.LICENSE 	new file:   build/static/js/2.39360e90.chunk.js.map 	new file:   build/static/js/main.cede48e4.chunk.js 	new file:   build/static/js/main.cede48e4.chunk.js.map 	new file:   build/static/js/runtime-main.9642a01f.js 	new file:   build/static/js/runtime-main.9642a01f.js.map

---
+++ build/precache-manifest.1d272981a9d3f405d01dfd8f524438e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa2f4b41060a600d7ca9e3f0847c4767",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6d47a5909934d494e69",
+    "url": "/static/css/main.eae4d89b.chunk.css"
+  },
+  {
+    "revision": "ded2e5346fe4f2ee5fea",
+    "url": "/static/js/2.39360e90.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.39360e90.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a6d47a5909934d494e69",
+    "url": "/static/js/main.cede48e4.chunk.js"
+  },
+  {
+    "revision": "f912d8a45f07caf56953",
+    "url": "/static/js/runtime-main.9642a01f.js"
+  }
+]);
